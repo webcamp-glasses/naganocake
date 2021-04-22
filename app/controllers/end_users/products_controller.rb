1add17ff5ec04fff3eacedef650415363a5e98cd
24wkt <05wkt38xy.waku@gmail.com>
2021-04-22 04:18:34 +0000

    [add & update]新規会員登録画面の編集と階層の編集

--- app/controllers/end_users/products_controller.rb
+++
@@ -1,7 +0,0 @@
-class EndUsers::ProductsController < ApplicationController
-  def index
-  end
-
-  def show
-  end
-end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  
+  def full_name
+    (last_name + first_name).to_s
+  end
 end

4f9f231f9ffae37107c811fe713234a3acbc37c3
mf-2021 <manamu.fujiwara@gmail.com>
2021-04-23 07:33:19 +0000

    [Add:end_users]products-show

--- app/controllers/end_user/products_controller.rb
+++ app/controllers/end_user/products_controller.rb
@@ -3,5 +3,6 @@ class EndUser::ProductsController < ApplicationController
   end
 
   def show
+    @product = Product.find(params[:id])
   end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '5cce807456358e9f59ce650e3b7bf3dc83655b8ee664f6b4b76df00c20d301874093d69a0682f6693efe8beda1c00910cdba64a8e0141e8c5a207d0ae32d962d'

1da3e847a8adde5bddcbb59c642635bace087f71
SHUNJI1018 <hide971gg@gmail.com>
2021-04-25 07:21:12 +0000

    [debug] 一回目

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2021_04_22_104708) do
 
   create_table "addresses", force: :cascade do |t|
-    t.integer "user_id"
+    t.integer "end_user_id"
     t.string "name"
     t.string "postal_code"
     t.string "address"
@@ -51,7 +51,7 @@ ActiveRecord::Schema.define(version: 2021_04_22_104708) do
     t.string "postal_code"
     t.string "address"
     t.string "phone_number"
-    t.boolean "is_deleted", default: false
+    t.boolean "is_deleted", default: false, null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
